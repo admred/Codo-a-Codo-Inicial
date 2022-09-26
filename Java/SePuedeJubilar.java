ecbb7b36f2487990214a990b86e2ccb34de5bbeb
user <user@MCP61>
2022-09-25 22:04:28 -0300

    ops ...

--- Java/SePuedeJubilar.java
+++ Java/SePuedeJubilar.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 
-class SePuedeJubilar {
 
-    public static void main(String[] args) {
-        int edad;
-        String sexo;
+public class SePuedeJubilar {
+	 public static void main(String[] args) {
+	 int edad;
+     char sexo;
 
-        System.out.println("Ingresar Sexo:");
-        Scanner leer = new Scanner(System.in) ;
-        sexo=leer.nextLine();
+     System.out.println("Ingresar Sexo:");
+     Scanner leer = new Scanner(System.in) ;
+     sexo=leer.next().charAt(0);
+     leer.close();
 
-        System.out.println("Ingresar Edad:");
-        leer = new Scanner(System.in) ;
-        edad = leer.nextInt();
-
-        if ( sexo == "F" ||  sexo == "M" ) {
-
-
-        } else {
-
-        }
-        
-        leer.close();
-    }
+     System.out.println("Ingresar Edad:");
+     leer = new Scanner(System.in) ;
+     edad = leer.nextInt();
+     leer.close();
+     
+     
+     if ( ( sexo == 'f' && sexo == 'F') &&  edad >= 60 ) {
+    	 System.out.println("Se puede jubilar con sexo="+sexo+" y edad="+edad);
+    	 
+     } else if ( (sexo == 'm' || sexo == 'M') &&  edad >= 65 ) {
+    	 System.out.println("Se puede jubilar con sexo="+sexo+" y edad="+edad);
+    	 
+     } else {
+    	 System.out.println("No puede jubilar.");
+     }
+     
+	 }	
 }
-
